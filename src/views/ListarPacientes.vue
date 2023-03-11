0d643ffecb279ef4425fc3fd257c79de197482cc
eber.teles <eber.teles@cuboconnect.com>
2023-03-11 14:57:34 -0300

    feat(vue): subida inicial do framework.

---
+++ src/views/ListarPacientes.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Pacientes</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ListarPacientes"
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
