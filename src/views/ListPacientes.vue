f8443c3ee7d3f436e1f298524cd83dc4a9af527e
eber.teles <eber.teles@cuboconnect.com>
2023-03-13 20:43:25 -0300

    feat(paciente): paginação da listagem de pacientes.

--- src/views/ListPacientes.vue
+++ src/views/ListPacientes.vue
@@ -3,9 +3,9 @@
     <div class="row">
       <div class="col">
         <p class="h3 text-success fw-bold">Pacientes
-          <router-link to="/add/" class="btn btn-success btn-sm"><i class="fa fa-plus-circle"></i> Novo</router-link>
+          <router-link to="/add/" class="btn btn-success btn-sm"><i class="fa fa-plus-circle"></i> Novo</router-link>&nbsp;&nbsp;
+          <router-link to="/import/" class="btn btn-success btn-sm"><i class="fa fa-arrow-circle-up"></i> Importar</router-link>
         </p>
-
         <form>
           <div class="row">
             <div class="col-md-6">
@@ -52,6 +52,15 @@
         </div>
       </div>
 
+    <nav v-if="paginacao" class="align-content-center">
+      <ul class="pagination pagination justify-content-center">
+        <li class="page-item"><a class="page-link" href="#" v-on:click="pagina($event)" :id="paginacao.first" v-if="paginacao.first">Início</a></li>
+        <li class="page-item"><a class="page-link" href="#" v-on:click="pagina($event)" :id="paginacao.prev" v-if="paginacao.prev">Anterior</a></li>
+        <li class="page-item"><a class="page-link" href="#" v-on:click="pagina($event)" :id="paginacao.next" v-if="paginacao.next">Próximo</a></li>
+        <li class="page-item"><a class="page-link" href="#" v-on:click="pagina($event)" :id="paginacao.last" v-if="paginacao.last">Final</a></li>
+      </ul>
+    </nav>
+
   </div>
 
 </template>
@@ -61,13 +70,28 @@
     data() {
       return {
         listagem: this.getListagem(),
+        paginacao: {'first': null, 'last': null, 'prev': null, 'next': null},
       };
     },
     methods: {
       getListagem() {
         this.$http.get('/pacientes/').then((response)=>{
-          this.listagem = response.data.data;
+          this.setaDados(response);
         })
+      },
+      pagina(event) {
+        this.$http.get('/pacientes?page='+event.currentTarget.id).then((response)=>{
+          this.setaDados(response);
+        })
+      },
+      setaDados(response) {
+        this.paginacao =  {'first': null, 'last': null, 'prev': null, 'next': null};
+        this.listagem = response.data.data;
+        console.log(response.data.links.first);
+        if(response.data.links.first) this.paginacao.first = response.data.links.first.substring(response.data.links.first.indexOf("=") + 1);
+        if(response.data.links.last) this.paginacao.last = response.data.links.last.substring(response.data.links.last.indexOf("=") + 1);
+        if(response.data.links.prev) this.paginacao.prev = response.data.links.prev.substring(response.data.links.prev.indexOf("=") + 1);
+        if(response.data.links.next) this.paginacao.next = response.data.links.next.substring(response.data.links.next.indexOf("=") + 1);
       }
     }
   };
